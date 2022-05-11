09a59a63f51a909e00b002ec1d9c609bded4f0f4
joão <joao_saraiva@edu.unifor.br>
2022-05-11 00:27:15 -0300

    using socket io to define the winner of rock paper and scissor

--- app.js
+++ app.js
@@ -1,38 +1,65 @@
 const express = require("express");
 const app = express();
-var cors = require('cors')
+const cors = require('cors')
 const http = require('http');
+
 const { Server } = require("socket.io");
 const port = 3000;
-var bodyParser = require('body-parser');
-const { isObject } = require("util");
+const bodyParser = require('body-parser');
 const server = http.createServer(app);
-
-const io = new Server(server, {
-    // options
-});
-
-io.sockets.emit("hi", "everyone");
-
-io.on("connection", (socket) =>{
-
-})
-
 
 app.use(cors());
 app.use(bodyParser.json())
 
-var choices = [];
+let playesOptions = [];
 
-app.get('/', (req, res) => {
-    res.json({msg: "isos aew"})
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+    }
+});
+
+io.on("connection", (socket) =>{
+    socket.on('selectedOption', (data) =>{
+        let user = {id: socket.id};
+        if(playesOptions.filter(e => e.id == socket.id).length > 0){
+            socket.emit('alreadyChoosed', {message: 'You Already Have Choosed an Option'});
+        }else{
+            let play = {...data, ...user}
+            playesOptions.push(play);
+            io.emit('receivedOption', play);
+
+            if(playesOptions.length == 2){
+                let result = decideWinner(playesOptions);
+                io.emit('result', result);
+                playesOptions = [];
+            }
+        }
+    })
 })
 
-app.post('/choices/new', (req, res) => {
-    choices.push(req.body)
-    console.log(choices);
-    res.json({msg: "succesful"});
-})
+function decideWinner(playersOptions){
+    let playerOneOption = playersOptions[0].option
+    let playerTwoOption = playersOptions[1].option
+
+    if( ( playerOneOption.toLowerCase() == 'paper' || playerTwoOption.toLowerCase() == 'paper' ) && ( playerOneOption.toLowerCase() == 'rock' || playerTwoOption.toLowerCase() == 'rock' ) ){
+        return {
+            result: `Winner is: ${playerOneOption.toLowerCase() == 'paper' ? playersOptions[0].id : playersOptions[1].id}`
+        }
+    }else if( ( playerOneOption.toLowerCase() == 'rock' || playerTwoOption.toLowerCase() == 'rock' ) && ( playerOneOption.toLowerCase() == 'scissor' || playerTwoOption.toLowerCase() == 'scissor' ) ){
+        return {
+            result: `Winner is: ${playerOneOption.toLowerCase() == 'rock' ? playersOptions[0].id : playersOptions[1].id}`
+        }
+    }else if( ( playerOneOption.toLowerCase() == 'scissor' || playerTwoOption.toLowerCase() == 'scissor' ) && ( playerOneOption.toLowerCase() == 'paper' || playerTwoOption.toLowerCase() == 'paper' ) ){
+        return {
+            result: `Winner is: ${playerOneOption.toLowerCase() == 'scissor' ? playersOptions[0].id : playersOptions[1].id}`
+        }
+    }else{
+        return {
+            result: 'draw'
+        }
+    }
+}
 
 server.listen(port, () => {
     console.log("listen")
