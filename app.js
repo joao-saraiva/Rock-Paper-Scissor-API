393c46c09cada8db995f1a93de4276e010c49fc2
João <programador09@speedgov.com.br>
2022-05-10 21:45:22 -0300

    using socket io

--- app.js
+++ app.js
@@ -1,18 +1,39 @@
 const express = require("express");
 const app = express();
 var cors = require('cors')
+const http = require('http');
+const { Server } = require("socket.io");
 const port = 3000;
+var bodyParser = require('body-parser');
+const { isObject } = require("util");
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    // options
+});
+
+io.sockets.emit("hi", "everyone");
+
+io.on("connection", (socket) =>{
+
+})
+
 
 app.use(cors());
+app.use(bodyParser.json())
 
-app.get("/teste", (req, res) => {
-    res.json({msg: "uepa"})
-})
+var choices = [];
 
 app.get('/', (req, res) => {
-    res.send("maluco beleza")
+    res.json({msg: "isos aew"})
 })
 
-app.listen(port, () => {
+app.post('/choices/new', (req, res) => {
+    choices.push(req.body)
+    console.log(choices);
+    res.json({msg: "succesful"});
+})
+
+server.listen(port, () => {
     console.log("listen")
 })
